0fb05c42ca7385aba9bf50fd42eb41ac672cdb3c
rmazzine <mazzine.r@gmail.com>
2022-07-07 14:23:14 +0200

    Improve docstrings

--- cfnow/_img_segmentation.py
+++ cfnow/_img_segmentation.py
@@ -1,7 +1,19 @@
+"""
+This script has the strategies to segment images
+"""
 from skimage.segmentation import quickshift
 
 
 def gen_quickshift(img, params_seg):
+    """
+    Simple wrapper of the Scikit-Image quickshift algorithm
+    :param img: Image to be segmented
+    :type img: np.ndarray
+    :param params_seg: Parameters to be passed to the segmentation algorithm
+    :type params_seg: dict
+    :return: An array with a segment number for each pixel
+    :rtype: np.ndarray
+    """
     if params_seg is None:
         params_seg = {'kernel_size': 4, 'max_dist': 200, 'ratio': 0.2}
 
