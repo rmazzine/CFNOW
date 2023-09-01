145450f8877c4fdb7e9c97c791cc3b386837b0c1
rmazzine <mazzine.r@gmail.com>
2023-09-01 09:03:28 -0300

    Correct setup.py

--- setup.py
+++ setup.py
@@ -8,17 +8,18 @@ REQUIREMENTS = [r.replace('\n', '') for r in open('requirements.txt').readlines(
 
 setup_args = {
     'name': 'CFNOW',
-    'version': '0.0.1',
+    'version': '0.0.2',
     'description': 'Generate counterfactuals with ease. This package takes a model and point (with a certain class) '
                    'and minimally changes it to flip the classification result.',
     'long_description_content_type': 'text/markdown',
     'long_description': f'{README}\n{HISTORY}',
     'license': 'MIT',
     'package_dir': {'cfnow':''},
-    'packages': find_packages(),
+    'packages': find_packages(exclude=('tests\*', 'imgs\*')),
     'author': 'Raphael Mazzine Barbosa de Oliveira',
     'author_email': 'mazzine.r@gmail.com',
-    'url': '',
+    'url': 'https://github.com/rmazzine/CFNOW',
+    'download_url': 'https://pypi.org/project/cfnow/',
     'keywords': [
         'counterfactuals',
         'counterfactual explanations',
