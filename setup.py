212fe42c014a9c9e5a0d72ed9d6bc33b6bbfceb8
rmazzine <mazzine.r@gmail.com>
2023-09-01 09:21:45 -0300

    Include pkl data

--- setup.py
+++ setup.py
@@ -8,13 +8,17 @@ REQUIREMENTS = [r.replace('\n', '') for r in open('requirements.txt').readlines(
 
 setup_args = {
     'name': 'cfnow',
-    'version': '0.0.3',
+    'version': '0.0.4',
     'description': 'Generate counterfactuals with ease. This package takes a model and point (with a certain class) '
                    'and minimally changes it to flip the classification result.',
     'long_description_content_type': 'text/markdown',
     'long_description': f'{README}\n{HISTORY}',
     'license': 'MIT',
     'packages': find_packages(exclude=('tests\*', 'imgs\*')),
+    'include_package_data': True,
+    'package_data': {
+        '': ['*.pkl'],
+    },
     'author': 'Raphael Mazzine Barbosa de Oliveira',
     'author_email': 'mazzine.r@gmail.com',
     'url': 'https://github.com/rmazzine/CFNOW',
